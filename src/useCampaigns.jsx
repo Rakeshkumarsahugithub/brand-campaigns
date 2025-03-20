1d667f0d1f986c0f8a6ed09a32b028b8fe22aa6f
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-03-20 21:32:52 +0530

    Update useCampaigns.jsx

--- src/useCampaigns.jsx
+++ src/useCampaigns.jsx
@@ -37,7 +37,7 @@ const useCampaigns = () => {
             id: 3,
             brand: {
               name: "Puma",
-              logo: "https://i5.walmartimages.com/asr/b43a2299-f335-419…cf6e88ae4_1.33fec6a186e76c1d2dc4e54dfde895e4.jpeg",
+              logo: "https://i.pinimg.com/736x/59/80/0b/59800be1b9b2f9611063c04ac8832619.jpg",
               description: "Shop the latest Puma t-shirts and enjoy up to 30% refunds on any damages."
             }, //"https://sharkshirts.in/wp-content/uploads/2023/07/Puma-Oversized-T-Shirt-scaled.jpg"
             payout: { type: "refunds", value: "₹3000 - ₹5000 " },
